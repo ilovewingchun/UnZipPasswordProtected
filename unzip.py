95f741107188625065d17713700f18aca56374bb
Tyler <alphaone.tw@gmail.com>
2015-09-21 16:27:34 +0800

    better reporting

--- unzip.py
+++ unzip.py
@@ -7,20 +7,37 @@
 	Example: python unzip.py *.zip
 '''
 import sys
+import os
+import glob
 from zipfile import ZipFile
 file_fullname_list = sys.argv[1:]
 file_basename_list = []
 password_list = []
+#Change this to your own keyword.
+keyword = "infected"
+#Change this to match the last(or beginning) of N character from filename.
+position = -4
+
+print " [*] This script will try to unzip file(s) using following password encryption algorithm:"
+print "     Keyword '%s' + last %s filename charcaters." %(keyword,abs(position))
+print "     If the file doesn't require any password, it will still be unzipped."
 if len(sys.argv[1:]) == 0:
-	print "[ - ] Error: You need to specify at least one zip file."
+	print " [-] Error: You need to specify at least one zip file."
 	sys.exit()
 for i in file_fullname_list:
 	file_basename_list.append(i.split(".")[-2])
 for i in file_basename_list:
-    password_list.append(i[-4:])
-password_list = ["infected" + s for s in password_list ]
+    password_list.append(i[position:])
+password_list = [keyword + s for s in password_list ]
+
 for i in range(len(password_list)):
-	print "[ + ] Extracting malware sample %d/%d" %(i+1,len(password_list))
+	print " [+] Extracting malware sample %d/%d" %(i+1,len(password_list))
 	ZipFile(file_fullname_list[i]).extractall(pwd=password_list[i])
-	print "[ + ] Malware sample %s extracted, moving on to next one...\n" % file_basename_list[i]
-print "[ + ] All files extracted, good bye!\n"
+	print " [+] Malware sample %s extracted." % file_basename_list[i]
+	print " [*] Deleting %s!" % file_fullname_list[i]
+	try:
+		os.remove(os.path.abspath(file_fullname_list[i]))
+	except Exception, e:
+		print "Error: %s - %s." % (e.filename, e.strerror)
+	print " [+] Moving on to next one...\n"
+print " [+] All files extracted, good bye!\n"
