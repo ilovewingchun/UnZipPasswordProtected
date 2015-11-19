ae74a5ce3c415ed2268fc281b26ebd03b8aa0d9c
Tyler <alphaone.tw@gmail.com>
2015-11-20 00:52:05 +0800

    modifying comments

--- unzip.py
+++ unzip.py
@@ -18,9 +18,10 @@ keyword = "infected"
 #Change this to match the last(or beginning) of N character from filename.
 position = -4
 
-print " [*] This script will try to unzip file(s) using following password encryption algorithm:"
-print "     Keyword '%s' + last %s filename charcaters." %(keyword,abs(position))
-print "     If the file doesn't require any password, it will still be unzipped."
+print "-"*80
+print "This script will try to unzip file(s) using following password encryption algorithm:"
+print "Keyword '%s' + last %s filename charcaters." %(keyword,abs(position))
+print "If the file doesn't require any password, it will still be unzipped."
 if len(sys.argv[1:]) == 0:
 	print " [-] Error: You need to specify at least one zip file."
 	sys.exit()
@@ -31,13 +32,22 @@ for i in file_basename_list:
 password_list = [keyword + s for s in password_list ]
 
 for i in range(len(password_list)):
-	print " [+] Extracting malware sample %d/%d" %(i+1,len(password_list))
-	ZipFile(file_fullname_list[i]).extractall(pwd=password_list[i])
-	print " [+] Malware sample %s extracted." % file_basename_list[i]
-	print " [*] Deleting %s!" % file_fullname_list[i]
+	print "[+] Extracting malware sample %d/%d" %(i+1,len(password_list))
+	try:
+		ZipFile(file_fullname_list[i]).extractall(pwd=password_list[i])
+		print "[+] Malware sample %s extracted." % file_basename_list[i]
+		print "[*] Deleting %s!" % file_fullname_list[i]
+	except Exception, e:
+		print "-"*80
+		print "[-] Error: %s - %s." % (e.filename, e.strerror)
+		sys.exit()
+	# Removing OS path
 	try:
 		os.remove(os.path.abspath(file_fullname_list[i]))
 	except Exception, e:
-		print "Error: %s - %s." % (e.filename, e.strerror)
-	print " [+] Moving on to next one...\n"
-print " [+] All files extracted, good bye!\n"
+		print "-"*80
+		print "[-] Error: %s - %s." % (e.filename, e.strerror)
+		sys.exit()
+	print "[+] Moving on to next one...\n"
+print "-"*80
+print "[+] %s files extracted, good bye!\n" %len(password_list)
